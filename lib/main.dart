b7cc48727a69a48213f1bd5ce69f3f9178bf244d
unknown <shahidkahn11@gmail.com>
2021-09-06 03:18:45 -0700

    multiple child layout widget

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // mainAxisSize: MainAxisSize.min,
+            // verticalDirection: VerticalDirection.down,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+                ),
+
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 2'),
+                )
+            ]),
+        )
+      )
+    );
+  }
+}
