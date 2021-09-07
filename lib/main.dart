accaa4deb0f00ccf91c20538de59c442aa1a49d1
unknown <shahidkahn11@gmail.com>
2021-09-07 03:12:41 -0700

    Business Card App

--- lib/main.dart
+++ lib/main.dart
@@ -13,35 +13,58 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            // mainAxisSize: MainAxisSize.min,
-            // verticalDirection: VerticalDirection.down,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
-                ),
-
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 2'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/shahid.png'),
+              ),
+              Text('Shahid',
+                style: TextStyle(fontFamily:'Pacifico',
+                fontSize: 35.0,
+                color: Colors.white, 
+                fontWeight: FontWeight.bold)),
+              Text('FLUTTER DEVELOPER',
+              style:TextStyle(color: Colors.teal.shade100,
+              fontWeight: FontWeight.bold,
+              fontSize: 17.0,
+              fontFamily: 'SourceSansPro'
+              )),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child:Divider(
+                  color: Colors.teal.shade100,
                 )
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child:Padding(
+                  padding: EdgeInsets.all(3.0),
+                  child:ListTile(
+                    leading: Icon(Icons.phone,
+                  color: Colors.teal),
+                  title: Text('+92 313 456 789',
+                  style: TextStyle(color: Colors.teal,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0),),
+                  )
+                  ) 
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child:Padding(
+                  padding: EdgeInsets.all(3.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email,
+                  color: Colors.teal),
+                  title: Text('shahidkahn11@gmail.com',
+                  style: TextStyle(color: Colors.teal,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0),),
+                  ),))
             ]),
         )
       )
